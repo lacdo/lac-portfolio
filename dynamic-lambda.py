1b83792b1c559911d3af4cdecbd32a55f21ac23e
Lac Do <lacvinhdo@gmail.com>
2018-04-11 00:42:27 +0100

    Update daymic lambda

--- dynamic-lambda.py
+++ dynamic-lambda.py
@@ -4,17 +4,21 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-static_bucket= s3.Bucket('static.skinnerslane.com')
-dynamic_bucket = s3.Bucket('dynamic.skinnerslane.com')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-dynamic_zip = StringIO.StringIO()
-dynamic_bucket.download_fileobj('dynamicBuild.zip', dynamic_zip)
+    static_bucket= s3.Bucket('static.skinnerslane.com')
+    dynamic_bucket = s3.Bucket('dynamic.skinnerslane.com')
 
-with zipfile.ZipFile(dynamic_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        static_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': 'NoneType'})
-        static_bucket.Object(nm).Acl().put(ACL='public-read')
+    dynamic_zip = StringIO.StringIO()
+    dynamic_bucket.download_fileobj('dynamicBuild.zip', dynamic_zip)
+
+    with zipfile.ZipFile(dynamic_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            static_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': 'NoneType'})
+            static_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hello from Lambda'
