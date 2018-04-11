b7ad988208b18c9ad8c79a813adbca28e86fe622
Lac Do <lacvinhdo@gmail.com>
2018-04-11 01:31:30 +0100

    Update daymic lambda

--- dynamic-lambda.py
+++ dynamic-lambda.py
@@ -6,19 +6,27 @@ import mimetypes
 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-west-2:528774710936:deployDynamicTopic')
 
-    static_bucket= s3.Bucket('static.skinnerslane.com')
-    dynamic_bucket = s3.Bucket('dynamic.skinnerslane.com')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    dynamic_zip = StringIO.StringIO()
-    dynamic_bucket.download_fileobj('dynamicBuild.zip', dynamic_zip)
+        static_bucket= s3.Bucket('static.skinnerslane.com')
+        dynamic_bucket = s3.Bucket('dynamic.skinnerslane.com')
 
-    with zipfile.ZipFile(dynamic_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            static_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': 'NoneType'})
-            static_bucket.Object(nm).Acl().put(ACL='public-read')
+        dynamic_zip = StringIO.StringIO()
+        dynamic_bucket.download_fileobj('dynamicBuild.zip', dynamic_zip)
 
-    return 'Hello from Lambda'
+        with zipfile.ZipFile(dynamic_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                static_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': 'NoneType'})
+                static_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        return "Job Done!"
+        topic.publish(Subject="Dynamic Website Deployed", Message="Dynamic Website Deployed Successfully!")
+    except:
+        topic.publish(Subject="Dynamic Website Deployed Failed", Message="Dynamic Website Was Not Deployed Successfully!")
+        raise
