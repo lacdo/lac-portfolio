ca6a2fa623c084b1113bfd0b3273efa95658094b
Lac Do <lacvinhdo@gmail.com>
2018-04-11 06:08:21 +0100

    Update daymic lambda

--- dynamic-lambda.py
+++ dynamic-lambda.py
@@ -42,7 +42,7 @@ def lambda_handler(event, context):
         topic.publish(Subject="Dynamic Website Deployed", Message="Dynamic Website Deployed Successfully!")
         if job:
             codepipeline = boto3.client('codepipeline')
-            codepipeline.put_job_success_result(job["id"])
+            codepipeline.PutJobSuccessResult(job["id"])
     except:
         topic.publish(Subject="Dynamic Website Deployed Failed", Message="Dynamic Website Was Not Deployed Successfully!")
         raise
