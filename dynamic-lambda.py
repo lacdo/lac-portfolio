ef6ae40661f2831f921a1f234f9300f493742586
Lac Do <lacvinhdo@gmail.com>
2018-04-12 16:56:11 +0100

    More commits

--- dynamic-lambda.py
+++ dynamic-lambda.py
@@ -1,11 +1,12 @@
 import boto3
-from botocore.client import Config
+# from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
 
 
 def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:eu-west-2:528774710936:deployDynamicTopic')
 
@@ -23,7 +24,7 @@ def lambda_handler(event, context):
 
         print "Building Dynamic Website from " + str(location)
 
-        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+#        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         static_bucket= s3.Bucket('static.skinnerslane.com')
         dynamic_bucket = s3.Bucket(location["bucketName"])
@@ -35,7 +36,7 @@ def lambda_handler(event, context):
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
                 static_bucket.upload_fileobj(obj, nm,
-                    ExtraArgs={'ContentType': 'NoneType'})
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 static_bucket.Object(nm).Acl().put(ACL='public-read')
 
         return "Job Done!"
