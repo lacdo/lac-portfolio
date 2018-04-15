90e5a2a14ede9653e05bf10e7783d59bf8e0e1a3
Lac Do <lacvinhdo@gmail.com>
2018-04-15 01:37:57 +0100

    lambda update

--- dynamic-lambda.py
+++ dynamic-lambda.py
@@ -3,16 +3,20 @@ from botocore.client import Config
 import StringIO
 import zipfile
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-skinnerslane_bucket= s3.Bucket('skinnerslane.com')
-build_bucket = s3.Bucket('build.skinnerslane.com')
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-build_zip = StringIO.StringIO()
-build_bucket.download_fileobj('buildSkinnerslane.zip', build_zip)
+    skinnerslane_bucket= s3.Bucket('skinnerslane.com')
+    build_bucket = s3.Bucket('build.skinnerslane.com')
 
-with zipfile.ZipFile(build_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        skinnerslane_bucket.upload_fileobj(obj, nm)
-        skinnerslane_bucket.Object(nm).Acl().put(ACL='public-read')
+    build_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('buildSkinnerslane.zip', build_zip)
+
+    with zipfile.ZipFile(build_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            skinnerslane_bucket.upload_fileobj(obj, nm)
+            skinnerslane_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hello from Lambda'
