e106c5b304fd5384850984bad963b48468ac4a1a
Lac Do <lacvinhdo@gmail.com>
2018-04-16 15:09:12 +0100

    deployLambda

--- uploadSkinnerLane.py
+++ uploadSkinnerLane.py
@@ -1,10 +1,11 @@
 import boto3
-from botocore.client import Config
+# from botocore.client import Config
 import StringIO
 import zipfile
-import mimetypes
+# import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+s3 = boto3.resource('s3')
+#s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
 skinnerslane_bucket= s3.Bucket('skinnerslane.com')
 build_bucket = s3.Bucket('build.skinnerslane.com')
