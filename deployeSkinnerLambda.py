a231e5cbcf90e9ef3a9089002846fcfde3c734e7
Lac Do <lacvinhdo@gmail.com>
2018-04-16 16:20:01 +0100

    last deplyed

--- deployeSkinnerLambda.py
+++ deployeSkinnerLambda.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
@@ -7,14 +8,28 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic =sns.Topic('arn:aws:sns:us-east-1:528774710936:deployeSkinnerTopic')
 
+    location = {
+        "bucketName": 'build.skinnerslane.com',
+        "objectKey": 'buildSkinnerslane.zip'
+    }
+
     try:
-        s3 = boto3.resource('s3')
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building Skinner's Lane from " + str(location)
+
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         skinnerslane_bucket= s3.Bucket('skinnerslane.com')
-        build_bucket = s3.Bucket('build.skinnerslane.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         build_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('buildSkinnerslane.zip', build_zip)
+        build_bucket.download_fileobj(location["objectKey"], build_zip)
 
         with zipfile.ZipFile(build_zip) as myzip:
             for nm in myzip.namelist():
@@ -25,6 +40,9 @@ def lambda_handler(event, context):
 
         print "Job done!"
         topic.publish(Subject="Deployment Completed", Message="Skinner's Lane deployed suscessfully!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Deployment Failed", Message="Skinner's Lane was not deployed successfully!")
         raise
