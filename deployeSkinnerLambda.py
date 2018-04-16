f19191b1d6de136ab34b92e083b82ab926266174
Lac Do <lacvinhdo@gmail.com>
2018-04-16 15:35:15 +0100

    deployLambda

--- deployeSkinnerLambda.py
+++ deployeSkinnerLambda.py
@@ -4,19 +4,29 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic =sns.Topic('arn:aws:sns:us-east-1:528774710936:deployeSkinnerTopic')
 
-    skinnerslane_bucket= s3.Bucket('skinnerslane.com')
-    build_bucket = s3.Bucket('build.skinnerslane.com')
+    try:
+        s3 = boto3.resource('s3')
 
-    build_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('buildSkinnerslane.zip', build_zip)
+        skinnerslane_bucket= s3.Bucket('skinnerslane.com')
+        build_bucket = s3.Bucket('build.skinnerslane.com')
 
-    with zipfile.ZipFile(build_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            skinnerslane_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': 'None-Type'})
-            skinnerslane_bucket.Object(nm).Acl().put(ACL='public-read')
+        build_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('buildSkinnerslane.zip', build_zip)
+
+        with zipfile.ZipFile(build_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                skinnerslane_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': 'None-Type'})
+                skinnerslane_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job done!"
+        topic.publish(Subject="Deployment Completed", Message="Skinner's Lane deployed suscessfully!")
+    except:
+        topic.publish(Subject="Deployment Failed", Message="Skinner's Lane was not deployed successfully!")
+        raise
 
     return 'Hello from Lambda'
